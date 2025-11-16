b6334bb2686d67999623d6e01574cbf0a9538494
cykimbb <cykimbb@gmail.com>
2025-11-16 18:56:12 +0900

    Feat: 로그인 페이지 UI 구현

--- Comma/src/pages/Login/LoginPage.jsx
+++ Comma/src/pages/Login/LoginPage.jsx
@@ -1,5 +1,23 @@
+import styles from './LoginPage.module.css';
+import serviceLogo from '../../assets/쉼_로고.svg'
+import kakaoLogo from '../../assets/카카오_로고.svg'
+
 function Login() {
-  return <h1>로그인 페이지 입니다.</h1>;
+  return (
+    <div className={styles.viewport}>
+      <div className={styles.container}>
+        <img className={styles.serviceLogo} src={serviceLogo}></img>
+        <div className={styles.guideText}>
+          <p><span className={styles.highlightText}>오늘의 소셜 에너지</span>를 분석하여</p>
+          <p><span className={styles.highlightText}>회복 루틴</span>을 추천해드릴게요.</p>
+        </div>
+        <button className={styles.btn}>
+          <img className={styles.kakaoLogo} src={kakaoLogo}></img>
+          카카오톡으로 시작하기
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Login;
