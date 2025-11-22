bffc34295ce4fda96fedb139ed289468fe4666d1
dbsgustmd <hyun136000@gmail.com>
2025-11-22 15:17:00 +0900

    Feat: main 페이지 css 수정

--- Comma/src/pages/Main/MainPage.jsx
+++ Comma/src/pages/Main/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import CalendarComponent from "./CalendarComponent";
 import styles from "./MainPage.module.css";
@@ -7,7 +7,22 @@ import mypageIcon from "../../assets/mypage-icon.svg";
 function MainPage() {
   const navigate = useNavigate();
   const [hasTodayRecord, setHasTodayRecord] = useState(false);
-
+  useEffect(() => {
+    // #root의 height를 없애고 싶을 때
+    const root = document.getElementById("root");
+    if (root) {
+      // 기존 height를 저장하고 초기화
+      root.dataset.prevHeight = root.style.height;
+      root.style.height = "auto"; // 또는 "auto"
+    }
+    return () => {
+      // 페이지 벗어날 때 height 복구
+      if (root && root.dataset.prevHeight !== undefined) {
+        root.style.height = root.dataset.prevHeight;
+        delete root.dataset.prevHeight;
+      }
+    };
+  }, []);
   return (
     <div className={styles.viewport}>
       <div className={styles.container}>
